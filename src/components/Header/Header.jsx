feeefb7fb6f1c48c5112ac95516c301379843c1a
shabdandev <arslanbekovshabdan1@gmail.com>
2024-06-30 00:53:53 +0600

    goot

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -101,7 +101,7 @@ function Header(props) {
                 color: "#fff",
                 width: "50px",
                 height: "50px",
-                marginLeft: "340px",
+                marginLeft: "325px",
               }}
             >
               <MenuIcon sx={{ fontSize: "32px" }} />
