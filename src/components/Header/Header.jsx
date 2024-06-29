4b62dcada90f4df75277f2c1a1b14969dc88aae1
shabdandev <arslanbekovshabdan1@gmail.com>
2024-06-30 01:05:49 +0600

    uuuuu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -101,7 +101,7 @@ function Header(props) {
                 color: "#fff",
                 width: "50px",
                 height: "50px",
-                marginLeft: "300px",
+                marginLeft: "290px",
               }}
             >
               <MenuIcon sx={{ fontSize: "32px" }} />
